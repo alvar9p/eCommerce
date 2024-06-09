0857a1d2b52451c56010709a5017f4e8cb3c0aa4
Alvaro Poblete <y5u@economical.com>
2024-06-09 11:18:26 -0400

    Initial commit

---
+++ ShopmeProject/src/main/java/com/shopme/ShopmeProject/ShopmeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.ShopmeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmeProjectApplication.class, args);
+	}
+
+}
---
+++ ShopmeProject/src/test/java/com/shopme/ShopmeProject/ShopmeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopme.ShopmeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
