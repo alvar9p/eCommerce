042f5b0b7efeed6d90ead26f2444e0979d4299fb
Alvaro Poblete <afpobleteg@gmail.com>
2024-12-25 19:30:12 -0300

    Added initial Entity and Repository code

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.admin.brand;
+
+import com.shopme.common.entity.Brand;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+}
