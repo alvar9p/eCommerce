94b83b2ef1f38eb23abace2e8a82c1a74947c7cf
Alvaro Poblete <afpobleteg@gmail.com>
2024-12-28 09:48:27 -0300

    Added code to create Brands

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/controller/BrandController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/controller/BrandController.java
@@ -1,7 +1,9 @@
 package com.shopme.admin.brand.controller;
 
 import com.shopme.admin.brand.service.BrandService;
+import com.shopme.admin.category.service.CategoryService;
 import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,11 +17,25 @@ public class BrandController {
     @Autowired
     private BrandService brandService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/brands")
     public String listAll(Model model){
         List<Brand> listBrands = brandService.listAll();
         model.addAttribute("listBrands", listBrands);
 
         return "brands/brands";
+    }
+
+    @GetMapping("/brands/new")
+    public String newBrand(Model model){
+        List<Category> listCategories = categoryService.listCategoriesUserInForm();
+
+        model.addAttribute("listCategories", listCategories);
+        model.addAttribute("brand", new Brand());
+        model.addAttribute("pageTitle", "Create New Brand");
+
+        return "brands/brand_form";
     }
 }
