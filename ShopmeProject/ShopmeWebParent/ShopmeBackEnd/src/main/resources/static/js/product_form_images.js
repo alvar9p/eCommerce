d3a83c065dee05cd0fa9d4ef444d50cb6ac3f2f4
Alvaro Poblete <afpobleteg@gmail.com>
2024-12-31 18:46:21 -0300

    Add details section added + code refactor to JS functions

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_images.js
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_images.js
@@ -1,20 +1,12 @@
 var extraImagesCount = 0;
-dropdownBrands = $("#brand");
-dropdownCategories = $("#category");
 
 $(document).ready(function () {
-    $("#shortDescription").richText();
-    $("#fullDescription").richText();
-
-    dropdownBrands.change(function () {
-        dropdownCategories.empty();
-        getCategories();
-    })
-    getCategories();
-
     $("input[name='extraImage']").each(function (index) {
         extraImagesCount++;
         $(this).change(function () {
+            if (!checkFileSize(this)) {
+                return;
+            }
             showExtraImageThumbnail(this, index);
         });
     });
@@ -61,40 +53,4 @@ function addNewExtraImageSection(index) {
 
 function removeExtraImage(index){
     $("#divExtraImage" + index).remove();
-}
-
-function getCategories() {
-    brandId = dropdownBrands.val();
-    url = brandModuleURL + "/" + brandId + "/categories";
-
-    $.get(url, function (responseJson) {
-        $.each(responseJson, function (index, category) {
-            $("<option>").val(category.id).text(category.name).appendTo(dropdownCategories);
-        })
-    });
-
-}
-
-function checkUnique(form) {
-    var productId = $("#id").val();
-    var productName = $("#name").val();
-    var csrfValue = $("input[name='_csrf']").val();
-
-    // Deben hacer match con los parametros que recibe el controlador
-    var params = { id: productId, name: productName, _csrf: csrfValue };
-
-    // checkUniqueurl viene del product_form
-    $.post(checkUniqueurl, params, function (response) {
-        if (response == "OK") {
-            form.submit();
-        } else if (response == "Duplicate") {
-            showWarningModal("There is another product having the same name " + productName);
-        } else {
-            showErrorModal("Unknown response from server");
-        }
-    }).fail(function () {
-        showErrorModal("Could not connect to the server");
-    })
-
-    return false;
 }
