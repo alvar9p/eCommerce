0764802a62a163fc1d29f3111a93b2c0659f8d7f
Alvaro Poblete <afpobleteg@gmail.com>
2024-12-30 21:15:52 -0300

    Added code to add extra images on Product form

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form.js
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form.js
@@ -1,3 +1,4 @@
+var extraImagesCount = 0;
 dropdownBrands = $("#brand");
 dropdownCategories = $("#category");
 
@@ -10,7 +11,57 @@ $(document).ready(function () {
         getCategories();
     })
     getCategories();
+
+    $("input[name='extraImage']").each(function (index) {
+        extraImagesCount++;
+        $(this).change(function () {
+            showExtraImageThumbnail(this, index);
+        });
+    });
 });
+
+function showExtraImageThumbnail(fileInput, index) {
+    var file = fileInput.files[0];
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $("#extraThumbnail" + index).attr("src", e.target.result);
+    };
+    reader.readAsDataURL(file);
+
+    if(index >= extraImagesCount - 1){
+        addNewExtraImageSection(index + 1);
+    }
+    
+}
+
+function addNewExtraImageSection(index) {
+    htmlExtraImage = `
+        <div class="col border m-3 p-2" id="divExtraImage${index}">
+            <div id="extraImageHeader${index}"><label>Extra Image #${index + 1}:</label></div>
+            <div class="m-2">
+                <img class="img-fluid" id="extraThumbnail${index}" alt="Extra image #${index + 1} preview" src="${defaultImageThumbnailSrc}" />
+            </div>
+            <div class="m-2">
+                <input type="file" name="extraImage" onchange="showExtraImageThumbnail(this, ${index})" accept="image/png, image/jpeg"/>
+            </div>
+        </div>
+    `;
+
+    htmlLinkRemove = `
+        <a class="btn fas fa-times-circle fa-2x icon-dark float-right" 
+            href="javascript:removeExtraImage(${index - 1})"
+            title="Remove this image"></a>
+    `;
+
+    $("#divProductImages").append(htmlExtraImage);
+
+    $("#extraImageHeader" + (index - 1)).append(htmlLinkRemove);
+    extraImagesCount++;
+}
+
+function removeExtraImage(index){
+    $("#divExtraImage" + index).remove();
+}
 
 function getCategories() {
     brandId = dropdownBrands.val();
